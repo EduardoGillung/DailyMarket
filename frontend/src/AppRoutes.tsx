ea1900bca2fc61531db9472807ae80cf096245ed
eduardogillung <gillung100@gmail.com>
2024-09-13 16:19:19 -0300

    header created

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Layout } from "@/layouts/layout";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/" element={<span> HOME PAGE</span>}/>
+            <Route path="/" element={<Layout>Home page</Layout>} />
+            <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 };
+
+export default AppRoutes;
