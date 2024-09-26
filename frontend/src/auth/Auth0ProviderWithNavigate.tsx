8c5d8201a1f216bd5c56bd9583467181395ae220
eduardogillung <gillung100@gmail.com>
2024-09-26 14:50:47 -0300

    requests working

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -5,6 +5,8 @@ type Props = {
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+    const { CreateUser } = useCreateMyUser();
+    
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
