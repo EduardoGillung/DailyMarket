29f2ad15b1a66911875f410f4d079b71040b6c7c
eduardogillung <gillung100@gmail.com>
2024-10-02 16:30:55 -0300

    309 jwtRequest complete

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,8 @@
+import { auth } from "express-oauth2-jwt-bearer";
+
+//Check the authorization token
+export const jwtCheck = auth({
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    tokenSigningAlg: 'RS256'
+  });
